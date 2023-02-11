359b58dcf788adfb3b53d1023a9b8139b36b7dff
Yash Oswal <yoswal@binghamton.edu>
2023-02-11 10:58:13 -0500

    Code Cleanup

--- submit/prj1-sol/decls.sh
+++ submit/prj1-sol/decls.sh
@@ -10,17 +10,10 @@ dir=`dirname $0`
 
 python3 $dir/pySol/main.py
 
-#<< com
-#echo $?
-#RETURN=$?
-#echo $RETURN
-#echo $?
 if [ $? -ne 0 ];
 then
-  #echo "The script myscript.sh was executed successfuly"
   exit 1
 else
-  #echo "The script myscript.sh was NOT executed successfuly and returned the code $RETURN"
   exit 0
 fi 
 #com
