98bbf30f3fdd019de258ba3c30c231ece862fbbd
Yash Oswal <yoswal@binghamton.edu>
2023-02-13 18:00:53 -0500

    Adding Absolute path

--- submit/prj1-sol/runTests.sh
+++ submit/prj1-sol/runTests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 extras=$HOME/cs571/projects/prj1/extras
-
+dir=$HOME/i571/submit/prj1-sol
 
 if [ -z "$1" ]
 then
-	$extras/do-tests.sh decls.sh
+	$extras/do-tests.sh $dir/decls.sh
 else
-	$extras/do-tests.sh decls.sh $extras/tests/$1
+	$extras/do-tests.sh $dir/decls.sh $extras/tests/$1
 fi
 	
